5e20459dd93f070feeed63e54f4d13a4eda84cae
ysl3000 <yannicklamprecht@live.de>
2020-02-18 13:45:38 +0100

    Cleanup

--- WorldBorderPlugin/src/main/java/com/github/yannicklamprecht/worldborder/plugin/WorldBorderPlugin.java
+++ WorldBorderPlugin/src/main/java/com/github/yannicklamprecht/worldborder/plugin/WorldBorderPlugin.java
@@ -2,7 +2,6 @@ package com.github.yannicklamprecht.worldborder.plugin;
 
 import com.github.yannicklamprecht.worldborder.api.BorderAPI;
 import com.github.yannicklamprecht.worldborder.api.WorldBorderApi;
-import com.github.yannicklamprecht.worldborder.api.WorldBorderApiImpl;
 import org.bukkit.Bukkit;
 import org.bukkit.plugin.java.JavaPlugin;
 
@@ -14,8 +13,7 @@ public class WorldBorderPlugin extends JavaPlugin {
     @Override
     public void onEnable() {
 
-        WorldBorderApiImpl worldBorderApi;
-        boolean supportsPersistence = false;
+        WorldBorderApi worldBorderApi;
 
         String version = Bukkit.getServer().getClass().getPackage().getName().replace('.', ',').split(",")[3];
         System.out.println("Version: " + version);
@@ -24,12 +22,10 @@ public class WorldBorderPlugin extends JavaPlugin {
                 worldBorderApi = new com.github.yannicklamprecht.worldborder.v1_13_R2.Impl();
                 break;
             case "v1_14_R1":
-                worldBorderApi = new com.github.yannicklamprecht.worldborder.v1_14_R1.Impl();
-                supportsPersistence = true;
+                worldBorderApi = new PersistenceWrapper(this, new com.github.yannicklamprecht.worldborder.v1_14_R1.Impl());
                 break;
             case "v1_15_R1":
-                worldBorderApi = new com.github.yannicklamprecht.worldborder.v1_15_R1.Impl();
-                supportsPersistence = true;
+                worldBorderApi = new PersistenceWrapper(this, new com.github.yannicklamprecht.worldborder.v1_15_R1.Impl());
                 break;
             default: {
                 getLogger().info("Unsupported version of Minecraft");
@@ -37,13 +33,7 @@ public class WorldBorderPlugin extends JavaPlugin {
                 return;
             }
         }
-        BorderAPI.setWorldBorderApi(wrapPersistence(worldBorderApi, supportsPersistence));
+        BorderAPI.setWorldBorderApi(worldBorderApi);
     }
 
-    private WorldBorderApi wrapPersistence(WorldBorderApiImpl worldBorderApi, boolean supportsPersistence) {
-        if(supportsPersistence){
-            return new PersistenceWrapper(this, worldBorderApi);
-        }
-        return worldBorderApi;
-    }
 }
