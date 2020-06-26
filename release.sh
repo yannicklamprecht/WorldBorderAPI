3631f40edba6d38a392cc5b8b36551318b3d5eec
ysl3000 <yannicklamprecht@live.de>
2020-06-26 22:07:09 +0200

    release script

--- release.sh
+++ release.sh
@@ -6,10 +6,12 @@ then
   exit 1
 fi
 
-git checkout origin/master
-git pull origin master
+git checkout master
+git fetch origin
+git reset origin/master --hard
 mvn versions:set -DnewVersion="$1"
 mvn versions:commit
 git commit -am "Release $1"
 git tag "$1"
 git push origin master --tags
+git reset origin/master --hard
