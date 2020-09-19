2e408261487ce7e937067889122096934db33f18
ysl3000 <yannicklamprecht@live.de>
2020-09-19 17:12:20 +0200

    changelog v2

--- release.sh
+++ release.sh
@@ -1,37 +1,31 @@
 #!/usr/bin/env bash
 
-color_from_hex(){
+color_from_hex() {
   curl -s -L -X GET "http://thecolorapi.com/id?hex=$1" | jq -r '.name.value'
 }
 
-get_hex_color_from_commit(){
+get_hex_color_from_commit() {
   git rev-parse --short=6 HEAD
 }
 
-get_changes(){
-  changes=$(git log --pretty="- %s" "$1"..master | grep --invert-match 'Update README.md')
-  printf "%q" "$changes"
+get_changes() {
+  printf "%q" "$(git log --pretty="- %s" "$1"..master | grep --invert-match 'Update README.md')"
 }
 
-add_latest_changes_to_changelog(){
-  old_tag=$1
-  new_tag=$2
-  release_name=$3
-  changes=$4
-  sed -i.bak -E "/^## \[${old_tag}\].*/i## [$new_tag] $release_name\\n\\n${changes}\n" CHANGELOG.md
+add_latest_changes_to_changelog() {
+  sed -i.bak -E "/^## \[$1\].*/i## [$2] $3\\n\\n$4\n" CHANGELOG.md
   sed -i.bak -E "s/\\$'//" CHANGELOG.md
   rm CHANGELOG.md.bak
 }
 
-if [[ $# -ne 1 ]]
-then
+if [[ $# -ne 1 ]]; then
   echo "$0 <version>"
   exit 1
 fi
 
 git checkout master
 git fetch origin
-# git reset origin/master --hard
+git reset origin/master --hard
 
 old_tag=$(git describe --abbrev=0 --tags "$(git rev-list --tags --skip=1 --max-count=1)")
 
@@ -40,11 +34,12 @@ release_name=$(color_from_hex "$(get_hex_color_from_commit)")
 
 add_latest_changes_to_changelog "$old_tag" "$1" "$release_name" "$changes"
 
-mvn versions:set -DnewVersion="$1"
-mvn versions:commit
 sed -i.bak -E "s/(<version>).*(<\/version>)/\1$1\2/" README.md
 rm README.md.bak
+
+mvn versions:set -DnewVersion="$1"
+mvn versions:commit
 git commit -am "Release $1"
 git tag "$1"
-# git push origin master --tags
-# git reset origin/master --hard
+git push origin master --tags
+git reset origin/master --hard
