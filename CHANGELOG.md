09e385018cb5b817a21f6c0731f08ce319b12008
ysl3000 <yannicklamprecht@live.de>
2020-09-19 16:57:04 +0200

    Release 1.163.1

--- CHANGELOG.md
+++ CHANGELOG.md
@@ -4,11 +4,6 @@ All notable changes to this project will be documented in this file.
 The format is based on [Keep a Changelog](https://keepachangelog.com/en/1.0.0/),
 and this project adheres to [Semantic Versioning](https://semver.org/spec/v2.0.0.html).
 
-## [1.163.0] Shamrock
-
-- updated the changelog manually the last time
-- automatically updating changelog'
-
 ## [1.163.0] Update to 1.16.3
 
 ## [1.161.0] The persistence update
--- release.sh
+++ release.sh
@@ -8,6 +8,21 @@ get_hex_color_from_commit(){
   git rev-parse --short=6 HEAD
 }
 
+get_changes(){
+  changes=$(git log --pretty="- %s" "$1"..master | grep --invert-match 'Update README.md')
+  printf "%q" "$changes"
+}
+
+add_latest_changes_to_changelog(){
+  old_tag=$1
+  new_tag=$2
+  release_name=$3
+  changes=$4
+  sed -i.bak -E "/^## \[${old_tag}\].*/i## [$new_tag] $release_name\\n\\n${changes}\n" CHANGELOG.md
+  sed -i.bak -E "s/\\$'//" CHANGELOG.md
+  rm CHANGELOG.md.bak
+}
+
 if [[ $# -ne 1 ]]
 then
   echo "$0 <version>"
@@ -16,18 +31,14 @@ fi
 
 git checkout master
 git fetch origin
-git reset origin/master --hard
+# git reset origin/master --hard
 
 old_tag=$(git describe --abbrev=0 --tags "$(git rev-list --tags --skip=1 --max-count=1)")
 
-changes=$(git log --pretty="- %s" "${old_tag}"..master | grep --invert-match 'Update README.md')
-
-escaped_changes="$(printf "%q" "$changes")"
-
+changes="$(get_changes "$old_tag")"
 release_name=$(color_from_hex "$(get_hex_color_from_commit)")
-sed -i.bak -E "/^## \[${old_tag}\].*/i## [$1] $release_name\\n\\n${escaped_changes}\n" CHANGELOG.md
-sed -i.bak -E "s/\\$'//" CHANGELOG.md
-rm CHANGELOG.md.bak
+
+add_latest_changes_to_changelog "$old_tag" "$1" "$release_name" "$changes"
 
 mvn versions:set -DnewVersion="$1"
 mvn versions:commit
@@ -35,5 +46,5 @@ sed -i.bak -E "s/(<version>).*(<\/version>)/\1$1\2/" README.md
 rm README.md.bak
 git commit -am "Release $1"
 git tag "$1"
-git push origin master --tags
-git reset origin/master --hard
+# git push origin master --tags
+# git reset origin/master --hard
