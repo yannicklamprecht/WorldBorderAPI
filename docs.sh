2daafd7b17969ea2d1946c222b4a45a520273efd
Yannick Lamprecht <yannicklamprecht@live.de>
2021-05-23 15:05:46 +0200

    migrate java docs generation script to gradle

--- docs.sh
+++ docs.sh
@@ -1,12 +1,9 @@
 #!/usr/bin/env bash
 
-export MAVEN_OPTS="-XX:+TieredCompilation -XX:TieredStopAtLevel=1"
-
-mvn clean install -P Impl -T 1C -DdependencyLocationsEnabled=false
+./gradlew api:build
 
 rm -rf docs/
 (
-cd API/ || exit
-mvn javadoc:javadoc
-cp -r target/site/apidocs ../docs
+./gradlew api:javadoc
+cp -r api/build/docs/javadoc/ docs/
 )
