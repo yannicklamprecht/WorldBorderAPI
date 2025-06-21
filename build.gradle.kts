44a3f5dc07f1e238e836daf1ed73bb3a57292a8e
Yannick Lamprecht <yannicklamprecht@live.de>
2025-06-21 03:07:30 +0200

    reactivate reobfuscation

--- build.gradle.kts
+++ build.gradle.kts
@@ -25,7 +25,7 @@ dependencies {
 
 tasks {
     // Run reobfJar on build
-//    build { dependsOn(reobfJar) }
+    build { dependsOn(reobfJar) }
 
     compileJava {
         options.encoding = Charsets.UTF_8.name()
